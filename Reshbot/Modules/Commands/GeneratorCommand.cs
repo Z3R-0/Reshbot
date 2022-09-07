526b836fd1fb8200492e02cd886524091c02a13a
Z3R-0 <timmeermans@live.nl>
2022-09-07 13:20:29 +0200

    Preparing to add new command

---
+++ Reshbot/Modules/Commands/GeneratorCommand.cs
@@ -0,0 +1,11 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Reshbot.ReshDiscordUtils;
+using Reshbot.SQLModels;
+
+namespace Reshbot.Modules.Commands {
+    public class GeneratorCommand : BotInteraction<SocketSlashCommand> {
+
+    }
+}
